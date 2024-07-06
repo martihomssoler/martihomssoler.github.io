6239f079f07b89d1f9737ec5e753008f85eb4a8c
martihomssoler <martihomssoler@gmail.com>
2024-07-06 17:52:03 +0200

    add two_of_us keyboard+layout

--- shell.nix
+++ shell.nix
@@ -1,12 +1,21 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell rec {
-  # nativeBuiltInputs = with pkgs; [
-  # ];
+
   buildInputs = with pkgs; [
     gcc
     stdenv.cc
     stdenv.cc.libc stdenv.cc.libc_dev
-    ruby_3_3
-    rubyPackages_3_3.jekyll
+    ruby
+    rubyPackages.jekyll
+    ruby.devEnv
+    git
+    sqlite
+    libpcap
+    postgresql
+    libxml2
+    libxslt
+    pkg-config
+    bundix
+    gnumake
   ];
 }
