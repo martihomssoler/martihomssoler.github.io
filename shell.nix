6b82dfb9ee2dc5e50499f588449e3f2b323ee609
martihomssoler <martihomssoler@gmail.com>
2025-04-15 08:54:03 +0200

    cleanup

--- shell.nix
+++ shell.nix
@@ -2,20 +2,11 @@
 pkgs.mkShell rec {
 
   buildInputs = with pkgs; [
-    gcc
-    stdenv.cc
-    stdenv.cc.libc stdenv.cc.libc_dev
-    ruby
-    rubyPackages.jekyll
-    ruby.devEnv
-    git
-    sqlite
-    libpcap
-    postgresql
-    libxml2
-    libxslt
-    pkg-config
-    bundix
-    gnumake
+    rustc
+    cargo
+    rustfmt
+    rust-analyzer
+    clippy
+    zola
   ];
 }
