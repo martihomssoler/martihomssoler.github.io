f2272b442a56ebc78752f177d4fa5145b564ea5a
martihomssoler <martihomssoler@gmail.com>
2025-04-17 07:50:26 +0200

    add initial theming and site design

---
+++ content/tech/_index.md
@@ -0,0 +1,6 @@
++++
+title = "MHS' Tech Posts"
+sort_by = "date"
+template = "tech.html"
+page_template = "tech-page.html"
++++
--- shell.nix
+++ shell.nix
@@ -1,12 +1,21 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell rec {
-
-  buildInputs = with pkgs; [
-    rustc
-    cargo
-    rustfmt
-    rust-analyzer
-    clippy
-    zola
+let
+  scripts = [
+    # TODO(mhs): fix this, it does not execute tailwindcss
+    (pkgs.writeShellScriptBin "serve" ''
+      trap 'kill 0' SIGINT; tailwindcss -i static/input.css -o public/output.css --watch & zola serve
+    '')
   ];
+in
+pkgs.mkShell rec {
+  buildInputs = with pkgs; [
+    # Rust packages
+    rustc cargo rustfmt rust-analyzer clippy
+    # Language server for html + css (and others)
+    vscode-langservers-extracted
+    # Tailwind css
+    tailwindcss
+    # Zola package
+    zola
+  ] ++ scripts;
 }
