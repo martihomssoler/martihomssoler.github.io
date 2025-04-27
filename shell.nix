73fcb448572b7b48f4531d1be08ad5ecd34a2f6e
martihomssoler <martihomssoler@gmail.com>
2025-04-28 00:10:55 +0200

    update shell.nix file with new commands

--- shell.nix
+++ shell.nix
@@ -1,12 +1,21 @@
 { pkgs ? import <nixpkgs> {} }:
 let
   scripts = with pkgs; [
-    (writeShellScriptBin "serve" ''
+    # serve the final static web a.k.a in release mode
+    (writeShellScriptBin "sr" ''
       trap 'kill 0' SIGINT; zola serve & tailwindcss -i static/input.css -o public/output.css --watch
     '')
-    # (writeShellScriptBin "sync" ''
-    #   echo ${./.sync-path}
-    # '')
+    # serve the static web with draft posts 
+    (writeShellScriptBin "sd" ''
+      trap 'kill 0' SIGINT; zola serve --drafts & tailwindcss -i static/input.css -o public/output.css --watch
+    '')
+    # reload the blog content, synching it with my local obsidian vault
+    (writeShellScriptBin "rl" ''
+      sync_path=${builtins.readFile ./.sync-path}
+
+      cp -r $sync_path/tech/* ./content/tech/
+      cp -r $sync_path/blog/* ./content/blog/
+    '')
   ];
 in
 pkgs.mkShell {
