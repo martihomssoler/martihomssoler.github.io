3c16529a333e880925548f565d993ff29499b47c
Martí Homs Soler <martihomssoler@gmail.com>
2026-09-25 13:35:46 +0200

    fix(zola): new syntax for highlighting code

--- flake.nix
+++ flake.nix
@@ -4,50 +4,52 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem ( system:
-    let
-    pkgs = (import nixpkgs { inherit system; });
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs { inherit system; });
 
-    nativeBuildInputs = with pkgs; [ pkg-config ];
+        nativeBuildInputs = with pkgs; [ pkg-config ];
 
-    buildInputs = with pkgs; [
-      # Zola package
-      zola
-    ];
+        buildInputs = with pkgs; [
+          # Zola package
+          zola
+        ];
 
-    devInputs = with pkgs; [
-      # Rust
-      rustup vscode-langservers-extracted
-      # Language server for html + css (and others)
-      vscode-langservers-extracted
-      # Tailwind css
-      tailwindcss_3
-      # Opencode
-      opencode
-      # Others
-      watchexec
-    ];
-    
-    in {
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = nativeBuildInputs ++ devInputs;
-        buildInputs = buildInputs ++ devInputs;
+        devInputs = with pkgs; [
+          # Rust
+          rustup
+          vscode-langservers-extracted
+          # Language server for html + css (and others)
+          vscode-langservers-extracted
+          # Tailwind css
+          tailwindcss_3
+          # Others
+          watchexec
+        ];
 
-        # https://github.com/rust-lang/rust-bindgen#environment-variables
-        LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath ( buildInputs ++ nativeBuildInputs );
-    
-        shellHook = ''
-          # Use binaries installed with `cargo install`
-          export PATH=$PATH:$CARGO_HOME/bin
-          export PROJECT_DIR=$(pwd)
-        '';
-      };
-  });
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = nativeBuildInputs ++ devInputs;
+          buildInputs = buildInputs ++ devInputs;
+
+          # https://github.com/rust-lang/rust-bindgen#environment-variables
+          LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (buildInputs ++ nativeBuildInputs);
+
+          shellHook = ''
+            # Use binaries installed with `cargo install`
+            export PATH=$PATH:$CARGO_HOME/bin
+            export PROJECT_DIR=$(pwd)
+          '';
+        };
+      }
+    );
 }
-
